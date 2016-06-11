7eb77c7ba77eb96a9566ae8b5d22b7d4a8c9a50d
Oliver Mansfield <O.Mansfield@amaze.com>
2016-06-11 10:56:59 +0100

    Adds date filter
    Adds function that lets you filter sessions by date and model

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
-var width = 960;
+var width = 1200;
 var height = 700;
 
 var frame = d3.select("body").append("svg")
             .attr("width", width)
-            .attr("height", height)
-            .style("background", "#494e53");
+            .attr("height", height);
 
 
 //Load in data file//
@@ -12,60 +11,26 @@ d3.json("js/data/sessions.json", function(data) {
 
 
 
-//Get amount of each Model selected, and put the number in an array//
-// var NumOfXE = [];
-// for (i = 0; i < data.length; i++){ 
-//     if (data[i].Model == "XE"){
-//         NumOfXE.push(data[i].Model)
-//     }
-// };
-// NumOfXE = NumOfXE.length;
-
-// var NumOfXF = [];
-// for (i = 0; i < data.length; i++){
-//     if (data[i].Model == "XF"){
-//         NumOfXF.push(data[i].Model)
-//     }
-// };
-// NumOfXF = NumOfXF.length;
-
-// var NumOfXJ = [];
-// for (i = 0; i < data.length; i++){
-//     if (data[i].Model == "XJ"){
-//         NumOfXJ.push(data[i].Model)
-//     }
-// };
-// NumOfXJ = NumOfXJ.length;
-
-// var NumOfFPace = [];
-// for (i = 0; i < data.length; i++){
-//     if (data[i].Model == "F-PACE"){
-//         NumOfFPace.push(data[i].Model)
-//     }
-// };
-// NumOfFPace = NumOfFPace.length;
-
-// function grow(carModel, numOfCarModel) {
-//   carModel.transition()
-//         .duration(2000)
-//         .attr('r', numOfCarModel *5 )
-// }
 
 //Uses lodash to get the amount of times each car model was selected from the data.
 //Then creates an 'amountOf' variable for each car model.
 function getModelCountById(data, id){
   var value = _.filter(data, function(item){
-    return item.Model == id;
+    return item.model == id;
   });
   return value.length;
-  console.log(value);
 }
-var amountOfXE = getModelCountById(data, "XE");
-var amountOfXF = getModelCountById(data, "XF");
-var amountOfXJ = getModelCountById(data, "XJ");
-var amountOfFPace = getModelCountById(data, "F-PACE");
 
-console.log(amountOfXE, amountOfXF, amountOfXJ, amountOfFPace)
+//Puts total number of models chosen into a variable. 
+var amountOfRangeRover = getModelCountById(data, "Range Rover");
+var amountOfRangeRoverSport = getModelCountById(data, "Range Rover Sport");
+var amountOfRangeRoverEvoque = getModelCountById(data, "Range Rover Evoque");
+var amountOfDiscovery = getModelCountById(data, "Discovery");
+var amountOfDiscoverySport = getModelCountById(data, "Discovery Sport");
+
+console.log(amountOfRangeRover, amountOfRangeRoverSport, amountOfRangeRoverEvoque, amountOfDiscovery, amountOfDiscoverySport)
+
+
 
 // var modelIds = ['XJ', 'F-PACE', 'XE', 'XF'];
 
@@ -77,20 +42,59 @@ console.log(amountOfXE, amountOfXF, amountOfXJ, amountOfFPace)
 // console.log(modelIds[0]);
 
 
-// var count = function(carModel, modelArray){
-//   var modelArray = [];
-//   for (i = 0; i < data.length; i++){
-//      if (data[i].Model == carModel){
-//         modelArray.push(data[i].Model)
-//     }
-//   };
-//   modelArray = modelArray.length
-// }
-
-// count(XE, NumOfXE);
 
 
-// console.log(NumOfXE, NumOfXF, NumOfXJ, NumOfFPace)
+function getDatedModelAmounts(model, startDay, endDay, startMonth, endMonth){
+    var filteredSession = 0; //Start with empty variable to hold counted sessions
+    for (i = 0; i < data.length; i++){ 
+        if (data[i].model == model //Filter by Car Model
+            && data[i].day >= startDay && data[i].day <= endDay //Filter by days
+            && data[i].month >= startMonth && data[i].month <= endMonth //Filter by months
+           )
+        {
+            filteredSession ++; //Increment variable for each session that matches filter criteria
+        }
+    };
+    someObject[model] = filteredSession; //Update object
+};
+
+//Object containing all amount of sessions which match the criteria,
+//this is to be read by the drawing code.
+var someObject = {
+    "Range Rover": 1,
+    "Range Rover Sport" : 1,
+    "Range Rover Evoque": 1,
+    "Discovery" : 1,
+    "Discovery Sport" : 1
+}
+
+//Call filtering function
+getDatedModelAmounts("Range Rover", 2, 20, 2, 6);
+getDatedModelAmounts("Range Rover Sport", 2, 20, 2, 6);
+getDatedModelAmounts("Range Rover Evoque", 2, 20, 2, 6);
+getDatedModelAmounts("Discovery", 2, 20, 2, 6);
+getDatedModelAmounts("Discovery Sport", 2, 20, 2, 6);
+
+console.log(someObject);
+
+//Look for amount of sessions that match the specific date and model//
+var filteredRangeRover = [];
+for (i = 0; i < data.length; i++){ 
+    if (data[i].model == "Range Rover" //Filter Car Model
+        && data[i].day >= 5 && data[i].day <= 28  //Filter days
+        && data[i].month >= 2 && data[i].month <= 6 //Filter months
+       )
+    {
+        filteredRangeRover.push(data[i].Model)
+    }
+};
+filteredRangeRover = filteredRangeRover.length;
+
+
+
+
+
+console.log(filteredRangeRover);
 
 
 
@@ -100,81 +104,65 @@ console.log(amountOfXE, amountOfXF, amountOfXJ, amountOfFPace)
 
 
 
+// myTextOptions[ 'character names' ].kid;
 
-// var circles = frame.selectAll('circle')
-//                     .data(data)
-//                     .enter()
-//                     .append('circle')
-//                     .attr("cy", 300)
-//                     .attr("cx", function(d){
-//                         if (d.Model == "XE"){
-//                             return 200;
-//                         } else if (d.Model == "XF"){
-//                             return 400;
-//                         } else if (d.Model == "XJ"){
-//                             return 600;
-//                         } else{
-//                             return 800;
-//                         }
-//                     })
-//                     .attr("r", function(d){
-//                         if (d.Model == "XE"){
-//                             return NumOfXE *2;
-//                         } else if (d.Model == "XF"){
-//                             return NumOfXF *2;
-//                         } else if (d.Model == "XJ"){
-//                             return NumOfXJ *2;
-//                         } else{
-//                             return NumOfFPace *2;
-//                         } 
-//                     })
-//                     .attr("fill", "red")
-
-
-var modelsXE = frame
+var modelsRangeRover = frame
                    .append('circle')
                    .attr('cy', 300)
                    .attr('cx', 100)
-                   .attr('r', 0)
+                   .attr('r', someObject['Range Rover'])
                    .attr('class', 'model-bubble');
 
-var modelsXF = frame
+var modelsRangeRoverSport = frame
                    .append('circle')
                    .attr('cy', 300)
                    .attr('cx', 300)
-                   .attr('r', 0)
+                   .attr('r', someObject['Range Rover Sport'])
                    .attr('class', 'model-bubble');
 
-var modelsXJ = frame
+var modelsRangeRoverEvoque = frame
                    .append('circle')
                    .attr('cy', 300)
                    .attr('cx', 500)
-                   .attr('r', 0)
+                   .attr('r', someObject['Range Rover Evoque'])
                    .attr('class', 'model-bubble');
 
-var modelsFPace = frame
+var modelsDiscovery = frame
                    .append('circle')
                    .attr('cy', 300)
                    .attr('cx', 700)
-                   .attr('r', 0)
+                   .attr('r', someObject['Discovery'])
+                   .attr('class', 'model-bubble');
+
+var modelsDiscoverySport = frame
+                   .append('circle')
+                   .attr('cy', 300)
+                   .attr('cx', 900)
+                   .attr('r', someObject['Discovery Sport'])
                    .attr('class', 'model-bubble');
 
 
 
 
 //Animates the radius of the model-bubbles
-//Takes carModel and numOfCarModel as parameters,
-//so you can invoke grow with any model.
-function grow(carModel, numOfCarModel) {
-  carModel.transition()
-        .duration(2000)
-        .attr('r', numOfCarModel *5 )
-}
+// Takes carModel and numOfCarModel as parameters,
+// so you can invoke grow with any model.
+// function grow(carModel, numOfCarModel) {
+//   carModel.transition()
+//         .duration(2000)
+//         .attr('r', numOfCarModel)
+// }
 
-grow(modelsXE, amountOfXE);
-grow(modelsXF, amountOfXF);
-grow(modelsXJ, amountOfXJ);
-grow(modelsFPace, amountOfFPace);
 
+
+//Calls animation
+// grow(modelsRangeRover, filteredRangeRover);
+// grow(modelsRangeRoverSport, filteredRangeRoverSport);
+// grow(modelsRangeRoverEvoque, filteredRangeRoverEvoque);
+// grow(modelsDiscovery, filteredDiscovery);
+// grow(modelsDiscoverySport, filteredDiscoverySport);
  
 });
+
+
+
