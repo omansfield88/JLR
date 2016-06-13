7272272c346aefd15da78e576f68480507e9930a
Oliver Mansfield <O.Mansfield@amaze.com>
2016-06-13 13:52:08 +0100

    Adds working date filter
    Changes date format YYYY-MM-DD. This is to ensure the date filtering
    works correctly.

--- js/script.js
+++ js/script.js
@@ -7,56 +7,9 @@ var frame = d3.select("body").append("svg")
 
 
 //Load in data file//
-d3.json("js/data/sessions.json", function(data) {
+d3.json("js/data/sessions-dated.json", function(data) {
 
 
-
-
-//Uses lodash to get the amount of times each car model was selected from the data.
-//Then creates an 'amountOf' variable for each car model.
-function getModelCountById(data, id){
-  var value = _.filter(data, function(item){
-    return item.model == id;
-  });
-  return value.length;
-}
-
-//Puts total number of models chosen into a variable. 
-var amountOfRangeRover = getModelCountById(data, "Range Rover");
-var amountOfRangeRoverSport = getModelCountById(data, "Range Rover Sport");
-var amountOfRangeRoverEvoque = getModelCountById(data, "Range Rover Evoque");
-var amountOfDiscovery = getModelCountById(data, "Discovery");
-var amountOfDiscoverySport = getModelCountById(data, "Discovery Sport");
-
-console.log(amountOfRangeRover, amountOfRangeRoverSport, amountOfRangeRoverEvoque, amountOfDiscovery, amountOfDiscoverySport)
-
-
-
-// var modelIds = ['XJ', 'F-PACE', 'XE', 'XF'];
-
-// for (var i = 0; i < modelIds.length; i++) {
-//   var id = modelIds[i];
-//   console.log('id: ' + id + ' value: ' +  getModelCountById(data, id));
-// }
-
-// console.log(modelIds[0]);
-
-
-
-
-function getDatedModelAmounts(model, startDay, endDay, startMonth, endMonth){
-    var filteredSession = 0; //Start with empty variable to hold counted sessions
-    for (i = 0; i < data.length; i++){ 
-        if (data[i].model == model //Filter by Car Model
-            && data[i].day >= startDay && data[i].day <= endDay //Filter by days
-            && data[i].month >= startMonth && data[i].month <= endMonth //Filter by months
-           )
-        {
-            filteredSession ++; //Increment variable for each session that matches filter criteria
-        }
-    };
-    someObject[model] = filteredSession; //Update object
-};
 
 //Object containing all amount of sessions which match the criteria,
 //this is to be read by the drawing code.
@@ -68,100 +21,163 @@ var someObject = {
     "Discovery Sport" : 1
 }
 
-//Call filtering function
-getDatedModelAmounts("Range Rover", 2, 20, 2, 6);
-getDatedModelAmounts("Range Rover Sport", 2, 20, 2, 6);
-getDatedModelAmounts("Range Rover Evoque", 2, 20, 2, 6);
-getDatedModelAmounts("Discovery", 2, 20, 2, 6);
-getDatedModelAmounts("Discovery Sport", 2, 20, 2, 6);
 
+console.log("data length " + data.length)
+
+//Date filter function//
+function getDatedModelAmounts(model, startDate, endDate){
+    var filteredSession = 0; //Start with empty variable to hold counted sessions
+    for (i = 0; i < data.length; i++){ 
+        if (data[i].model == model //Filter by Car Model
+            && data[i].date >= startDate && data[i].date <= endDate 
+            //&& data[i].day >= startDay && data[i].day <= endDay //Filter by days
+            //&& data[i].month >= startMonth && data[i].month <= endMonth //Filter by months
+           )
+        {
+            filteredSession ++; //Increment variable for each session that matches filter criteria
+        }
+    };
+    someObject[model] = filteredSession; //Update object
+};
+
+
+
+
+
+///////////////////////////////////////
+/////DRAW DEFAULT BUBBLES/////////////
+/////////////////////////////////////
+
+//Call filtering function to set the values of someObject
+
+getDatedModelAmounts("Range Rover", "2015-01-01", "2015-12-30");
+getDatedModelAmounts("Range Rover Sport", "2015-01-01", "2015-12-30");
+getDatedModelAmounts("Range Rover Evoque", "2015-01-01", "2015-12-30");
+getDatedModelAmounts("Discovery", "2015-01-01", "2015-12-30");
+getDatedModelAmounts("Discovery Sport", "2015-01-01", "2015-12-30");
 console.log(someObject);
 
-//Look for amount of sessions that match the specific date and model//
-var filteredRangeRover = [];
-for (i = 0; i < data.length; i++){ 
-    if (data[i].model == "Range Rover" //Filter Car Model
-        && data[i].day >= 5 && data[i].day <= 28  //Filter days
-        && data[i].month >= 2 && data[i].month <= 6 //Filter months
-       )
-    {
-        filteredRangeRover.push(data[i].Model)
-    }
-};
-filteredRangeRover = filteredRangeRover.length;
-
-
-
-
-
-console.log(filteredRangeRover);
-
-
-
-
-
-
-
-
-
-// myTextOptions[ 'character names' ].kid;
-
+//Draw model bubbles
 var modelsRangeRover = frame
-                   .append('circle')
-                   .attr('cy', 300)
-                   .attr('cx', 100)
-                   .attr('r', someObject['Range Rover'])
-                   .attr('class', 'model-bubble');
+                       .append('circle')
+                       .attr('cy', 300)
+                       .attr('cx', 100)
+                       .attr('r', 0)
+                       .attr('class', 'model-bubble');
 
 var modelsRangeRoverSport = frame
-                   .append('circle')
-                   .attr('cy', 300)
-                   .attr('cx', 300)
-                   .attr('r', someObject['Range Rover Sport'])
-                   .attr('class', 'model-bubble');
+                       .append('circle')
+                       .attr('cy', 300)
+                       .attr('cx', 300)
+                       .attr('r', 0)
+                       .attr('class', 'model-bubble');
 
 var modelsRangeRoverEvoque = frame
-                   .append('circle')
-                   .attr('cy', 300)
-                   .attr('cx', 500)
-                   .attr('r', someObject['Range Rover Evoque'])
-                   .attr('class', 'model-bubble');
+                       .append('circle')
+                       .attr('cy', 300)
+                       .attr('cx', 500)
+                       .attr('r', 0)
+                       .attr('class', 'model-bubble');
 
 var modelsDiscovery = frame
-                   .append('circle')
-                   .attr('cy', 300)
-                   .attr('cx', 700)
-                   .attr('r', someObject['Discovery'])
-                   .attr('class', 'model-bubble');
+                       .append('circle')
+                       .attr('cy', 300)
+                       .attr('cx', 700)
+                       .attr('r', 0)
+                       .attr('class', 'model-bubble');
 
 var modelsDiscoverySport = frame
-                   .append('circle')
-                   .attr('cy', 300)
-                   .attr('cx', 900)
-                   .attr('r', someObject['Discovery Sport'])
-                   .attr('class', 'model-bubble');
-
-
+                       .append('circle')
+                       .attr('cy', 300)
+                       .attr('cx', 900)
+                       .attr('r', 0)
+                       .attr('class', 'model-bubble');
 
 
 //Animates the radius of the model-bubbles
 // Takes carModel and numOfCarModel as parameters,
 // so you can invoke grow with any model.
-// function grow(carModel, numOfCarModel) {
-//   carModel.transition()
-//         .duration(2000)
-//         .attr('r', numOfCarModel)
-// }
-
-
+function grow(carModel, numOfCarModel) {
+  carModel.transition()
+        .duration(2000)
+        .attr('r', numOfCarModel)
+}
 
 //Calls animation
-// grow(modelsRangeRover, filteredRangeRover);
-// grow(modelsRangeRoverSport, filteredRangeRoverSport);
-// grow(modelsRangeRoverEvoque, filteredRangeRoverEvoque);
-// grow(modelsDiscovery, filteredDiscovery);
-// grow(modelsDiscoverySport, filteredDiscoverySport);
- 
+grow(modelsRangeRover, someObject['Range Rover']);
+grow(modelsRangeRoverSport, someObject['Range Rover Sport']);
+grow(modelsRangeRoverEvoque, someObject['Range Rover Evoque']);
+grow(modelsDiscovery, someObject['Discovery']);
+grow(modelsDiscoverySport, someObject['Discovery Sport']);
+
+
+
+
+
+////////////////////////////////////////////
+////////Update bubbles on date change//////
+//////////////////////////////////////////
+
+
+$("#dateFrom, #dateTo").change(function(){
+
+    console.log("changed");
+
+     //Remove all SVG elements first so they're not left on the page
+    //d3.selectAll("svg").remove();
+    var dateFrom = $("#dateFrom").val().split("");
+    var dateTo = $("#dateTo").val().split("");
+
+    //Reverses the date. 25-05-2015 turns into 2015-05-25
+    startDate = dateFrom[6] + dateFrom[7] + dateFrom[8] + dateFrom[9]
+                + dateFrom[5] + dateFrom[3] + dateFrom[4] 
+                + dateFrom[2] + dateFrom[0] + dateFrom[1];    
+    endDate = dateTo[6] + dateTo[7] + dateTo[8] + dateTo[9]
+                + dateTo[5] + dateTo[3] + dateTo[4] 
+                + dateTo[2] + dateTo[0] + dateTo[1];    
+
+
+    getDatedModelAmounts("Range Rover", startDate, endDate);
+    getDatedModelAmounts("Range Rover Sport", startDate, endDate);
+    getDatedModelAmounts("Range Rover Evoque", startDate, endDate);
+    getDatedModelAmounts("Discovery", startDate, endDate);
+    getDatedModelAmounts("Discovery Sport", startDate, endDate);
+
+    console.log(someObject);
+
+    //Animates the radius of the model-bubbles
+    // Takes carModel and numOfCarModel as parameters,
+    // so you can invoke grow with any model.
+    function grow(carModel, numOfCarModel) {
+      carModel.transition()
+            .duration(2000)
+            .attr('r', numOfCarModel)
+    }
+
+    //Calls animation
+    grow(modelsRangeRover, someObject['Range Rover']);
+    grow(modelsRangeRoverSport, someObject['Range Rover Sport']);
+    grow(modelsRangeRoverEvoque, someObject['Range Rover Evoque']);
+    grow(modelsDiscovery, someObject['Discovery']);
+    grow(modelsDiscoverySport, someObject['Discovery Sport']);
+     
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
 
 
