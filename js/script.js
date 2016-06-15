2692cc367203df9bb5513fceacb24fe67f2be8ae
Oliver Mansfield <O.Mansfield@amaze.com>
2016-06-15 12:06:46 +0100

    Adds nameplate counter
    Amended the date filter to look for the amount of nameplates associated
    with model in question. Still needs testing with different dates.

--- js/script.js
+++ js/script.js
@@ -1,44 +1,175 @@
 var width = 1200;
 var height = 700;
 
-var frame = d3.select("body").append("svg")
+var frame = d3.select(".frame-container").append("svg")
             .attr("width", width)
-            .attr("height", height);
+            .attr("height", height)
+            .attr("class", "svg-frame");
 
 
 //Load in data file//
-d3.json("js/data/sessions-dated.json", function(data) {
+d3.json("js/data/sessions-nameplates.json", function(data) {
+
+
+
+
+
 
 
 
 //Object containing all amount of sessions which match the criteria,
 //this is to be read by the drawing code.
-var someObject = {
-    "Range Rover": 1,
-    "Range Rover Sport" : 1,
-    "Range Rover Evoque": 1,
-    "Discovery" : 1,
-    "Discovery Sport" : 1
+
+var countedModels = {
+    "Range Rover": {
+        "Amount": 1,
+        "NPvogue": 1,
+        "NPvogueSE": 1,
+        "NPautobiography": 1,
+        "NPSVautobiography": 1
+    },
+    "Range Rover Sport": {
+        "Amount": 1,
+        "NPhse": 1,
+        "NPhseDynamic": 1,
+        "NPautobiographyDynamic": 1,
+        "NPsvr": 1
+    },
+    "Range Rover Evoque": {
+        "Amount": 1,
+        "NPse": 1,
+        "NPseTech": 1,
+        "NPhseDynamic": 1,
+        "NPhseDynamicLux": 1,
+        "NPautobiography": 1
+    },
+    "Discovery": {
+        "Amount": 1,
+        "NPgraphite": 1,
+        "NPlandmark": 1,
+        "NPcommercialSE": 1
+    },
+    "Discovery Sport": {
+        "Amount": 1,
+        "NPse": 1,
+        "NPseTech": 1,
+        "NPhse": 1,
+        "NPhseBlack": 1,
+        "NPhseLuxury": 1,
+        "NPhseDynamicLux": 1,
+    }
 }
 
-
-console.log("data length " + data.length)
 
 //Date filter function//
 function getDatedModelAmounts(model, startDate, endDate){
     var filteredSession = 0; //Start with empty variable to hold counted sessions
+    var NPvogue = 0;
+    var NPvogueSE = 0;
+    var NPautobiography = 0;
+    var NPSVautobiography = 0;
+    var NPautobiographyDynamic = 0;
+    var NPhse = 0;
+    var NPhseDynamic = 0;
+    var NPsvr = 0;
+    var NPse = 0;
+    var NPseTech = 0;
+    var NPhseDynamicLux = 0;
+    var NPgraphite = 0;
+    var NPlandmark = 0;
+    var NPcommercialSE = 0;
+    var NPhseBlack = 0;
+    var NPhseLuxury = 0;
+
     for (i = 0; i < data.length; i++){ 
         if (data[i].model == model //Filter by Car Model
-            && data[i].date >= startDate && data[i].date <= endDate 
-            //&& data[i].day >= startDay && data[i].day <= endDay //Filter by days
-            //&& data[i].month >= startMonth && data[i].month <= endMonth //Filter by months
+            && data[i].date >= startDate && data[i].date <= endDate //Filter by Date            
            )
         {
             filteredSession ++; //Increment variable for each session that matches filter criteria
         }
+
+        //Second if statement to check what the current sessions nameplate is,
+        //then increments a variable 
+        if (data[i].model == model){
+            if (data[i].nameplate == "Vogue"){
+                NPvogue ++; 
+            } 
+            else if (data[i].nameplate == "Vogue SE"){
+                NPvogueSE ++;
+            } 
+            else if (data[i].nameplate == "Autobiography"){
+                NPautobiography ++;           
+            } 
+            else if (data[i].nameplate == "SVAutobiography"){
+                NPSVautobiography ++;           
+            } 
+            else if (data[i].nameplate == "Autobiography Dynamic"){
+                NPautobiographyDynamic ++;           
+            } 
+            else if (data[i].nameplate == "HSE"){
+                NPhse ++;           
+            } 
+            else if (data[i].nameplate == "HSE Dynamic"){
+                NPhseDynamic ++;       
+            } 
+            else if (data[i].nameplate == "SVR"){
+                NPsvr ++;           
+            } 
+            else if (data[i].nameplate == "SE"){
+                NPse ++;
+            }
+            else if (data[i].nameplate == "SE Tech"){
+                NPseTech ++;           
+            } 
+            else if (data[i].nameplate == "HSE Dynamic Lux"){
+                NPhseDynamicLux ++
+            } 
+            else if (data[i].nameplate == "Graphite"){
+                NPgraphite ++;           
+            } 
+            else if (data[i].nameplate == "Landmark"){
+                NPlandmark ++;           
+            } 
+            else if (data[i].nameplate == "Commercial SE"){
+                NPcommercialSE ++;           
+            } 
+            else if (data[i].nameplate == "HSE Black"){
+                NPhseBlack ++;           
+            } 
+            else if (data[i].nameplate == "HSE Luxury"){
+                NPhseLuxury ++;           
+            }            
+        }
+
     };
-    someObject[model] = filteredSession; //Update object
+
+    //Updated object with the number of found models and nameplates
+    
+    countedModels[model]["Amount"] = filteredSession;
+    countedModels[model]["NPvogue"] = NPvogue;
+    countedModels[model]["NPvogueSE"] = NPvogueSE;
+    countedModels[model]["NPautobiography"] = NPautobiography;
+    countedModels[model]["NPSVautobiography"] = NPSVautobiography;
+    countedModels[model]["NPhse"] = NPhse;
+    countedModels[model]["NPhseDynamic"] = NPhseDynamic;
+    countedModels[model]["NPsvr"] = NPsvr;
+    countedModels[model]["NPse"] = NPse;
+    countedModels[model]["NPseTech"] = NPseTech;
+    countedModels[model]["NPhseDynamicLux"] = NPhseDynamicLux;
+    countedModels[model]["NPgraphite"] = NPgraphite;
+    countedModels[model]["NPlandmark"] = NPlandmark;
+    countedModels[model]["NPcommercialSE"] = NPcommercialSE;
+    countedModels[model]["NPhseBlack"] = NPhseBlack;
+    countedModels[model]["NPhseLuxury"] = NPhseLuxury;
 };
+
+
+
+
+
+
+console.log("data length " + data.length)
 
 
 
@@ -48,51 +179,67 @@ function getDatedModelAmounts(model, startDate, endDate){
 /////DRAW DEFAULT BUBBLES/////////////
 /////////////////////////////////////
 
-//Call filtering function to set the values of someObject
+//Call filtering function to set the values of countedModels
 
 getDatedModelAmounts("Range Rover", "2015-01-01", "2015-12-30");
 getDatedModelAmounts("Range Rover Sport", "2015-01-01", "2015-12-30");
 getDatedModelAmounts("Range Rover Evoque", "2015-01-01", "2015-12-30");
 getDatedModelAmounts("Discovery", "2015-01-01", "2015-12-30");
 getDatedModelAmounts("Discovery Sport", "2015-01-01", "2015-12-30");
-console.log(someObject);
+
+console.log(countedModels);
+
 
 //Draw model bubbles
 var modelsRangeRover = frame
                        .append('circle')
-                       .attr('cy', 300)
+                       .attr('cy', 340)
                        .attr('cx', 100)
                        .attr('r', 0)
+                       .on('click', function(d){
+                            console.log("hovered")
+                        })
                        .attr('class', 'model-bubble');
 
 var modelsRangeRoverSport = frame
                        .append('circle')
-                       .attr('cy', 300)
-                       .attr('cx', 300)
+                       .attr('cy', 340)
+                       .attr('cx', 340)
                        .attr('r', 0)
                        .attr('class', 'model-bubble');
 
 var modelsRangeRoverEvoque = frame
                        .append('circle')
-                       .attr('cy', 300)
-                       .attr('cx', 500)
+                       .attr('cy', 340)
+                       .attr('cx', 580)
                        .attr('r', 0)
                        .attr('class', 'model-bubble');
 
 var modelsDiscovery = frame
                        .append('circle')
-                       .attr('cy', 300)
-                       .attr('cx', 700)
+                       .attr('cy', 340)
+                       .attr('cx', 820)
                        .attr('r', 0)
                        .attr('class', 'model-bubble');
 
 var modelsDiscoverySport = frame
                        .append('circle')
-                       .attr('cy', 300)
-                       .attr('cx', 900)
+                       .attr('cy', 340)
+                       .attr('cx', 1060)
                        .attr('r', 0)
                        .attr('class', 'model-bubble');
 
+
+
+// .attr('r', someObject["Range Rover"]["npVogue"]);
+
+
+var nameplateDiscoverySportVogue = frame
+                       .append('circle')
+                       .attr('cy', 340)
+                       .attr('cx', 1060)
+                       .attr('r', 0)
+                       .attr('class', 'model-bubble');
 
 //Animates the radius of the model-bubbles
 // Takes carModel and numOfCarModel as parameters,
@@ -104,11 +251,11 @@ function grow(carModel, numOfCarModel) {
 }
 
 //Calls animation
-grow(modelsRangeRover, someObject['Range Rover']);
-grow(modelsRangeRoverSport, someObject['Range Rover Sport']);
-grow(modelsRangeRoverEvoque, someObject['Range Rover Evoque']);
-grow(modelsDiscovery, someObject['Discovery']);
-grow(modelsDiscoverySport, someObject['Discovery Sport']);
+grow(modelsRangeRover, countedModels['Range Rover']['Amount']);
+grow(modelsRangeRoverSport, countedModels['Range Rover Sport']['Amount']);
+grow(modelsRangeRoverEvoque, countedModels['Range Rover Evoque']['Amount']);
+grow(modelsDiscovery, countedModels['Discovery']['Amount']);
+grow(modelsDiscoverySport, countedModels['Discovery Sport']['Amount']);
 
 
 
@@ -121,20 +268,19 @@ grow(modelsDiscoverySport, someObject['Discovery Sport']);
 
 $("#dateFrom, #dateTo").change(function(){
 
-    console.log("changed");
-
-     //Remove all SVG elements first so they're not left on the page
-    //d3.selectAll("svg").remove();
+    //Get date from inside the input field
+    //Then split it into an array so it can be reversed
     var dateFrom = $("#dateFrom").val().split("");
     var dateTo = $("#dateTo").val().split("");
 
     //Reverses the date. 25-05-2015 turns into 2015-05-25
-    startDate = dateFrom[6] + dateFrom[7] + dateFrom[8] + dateFrom[9]
-                + dateFrom[5] + dateFrom[3] + dateFrom[4] 
-                + dateFrom[2] + dateFrom[0] + dateFrom[1];    
-    endDate = dateTo[6] + dateTo[7] + dateTo[8] + dateTo[9]
-                + dateTo[5] + dateTo[3] + dateTo[4] 
-                + dateTo[2] + dateTo[0] + dateTo[1];    
+    //This is so it works with the filter code
+    startDate = dateFrom[6] + dateFrom[7] + dateFrom[8] + dateFrom[9]//yyyy
+                + dateFrom[5] + dateFrom[3] + dateFrom[4] //mm
+                + dateFrom[2] + dateFrom[0] + dateFrom[1]; //dd
+    endDate = dateTo[6] + dateTo[7] + dateTo[8] + dateTo[9] //yyyy
+                + dateTo[5] + dateTo[3] + dateTo[4] //mm
+                + dateTo[2] + dateTo[0] + dateTo[1]; //dd
 
 
     getDatedModelAmounts("Range Rover", startDate, endDate);
@@ -143,7 +289,7 @@ $("#dateFrom, #dateTo").change(function(){
     getDatedModelAmounts("Discovery", startDate, endDate);
     getDatedModelAmounts("Discovery Sport", startDate, endDate);
 
-    console.log(someObject);
+    console.log(countedModels);
 
     //Animates the radius of the model-bubbles
     // Takes carModel and numOfCarModel as parameters,
@@ -155,16 +301,19 @@ $("#dateFrom, #dateTo").change(function(){
     }
 
     //Calls animation
-    grow(modelsRangeRover, someObject['Range Rover']);
-    grow(modelsRangeRoverSport, someObject['Range Rover Sport']);
-    grow(modelsRangeRoverEvoque, someObject['Range Rover Evoque']);
-    grow(modelsDiscovery, someObject['Discovery']);
-    grow(modelsDiscoverySport, someObject['Discovery Sport']);
+    grow(modelsRangeRover, countedModels['Range Rover']['Amount']);
+    grow(modelsRangeRoverSport, countedModels['Range Rover Sport']['Amount']);
+    grow(modelsRangeRoverEvoque, countedModels['Range Rover Evoque']['Amount']);
+    grow(modelsDiscovery, countedModels['Discovery']['Amount']);
+    grow(modelsDiscoverySport, countedModels['Discovery Sport']['Amount']);
      
 
 });
 
 
+///////////////////////////////////////////////////
+////////////Dive into nameplates//////////////////
+/////////////////////////////////////////////////
 
 
 
@@ -178,7 +327,8 @@ $("#dateFrom, #dateTo").change(function(){
 
 
 
-});
 
+
+}); //End of data loading function
 
 
